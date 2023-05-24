1675e765b09d6893722742af38d64aaa3701e55d
anamaricriscaravana <ana.caravana24@gmail.com>
2023-05-24 21:13:12 +0800

    Added List and Methods

--- RestaurantTableReservation.cs
+++ RestaurantTableReservation.cs
@@ -1,139 +1,163 @@
-﻿System.Console.WriteLine("\t HELLO! WELCOME TO RESTAURANT TABLE RESERVATION SYSTEM!");
-
-bool reserved1 = false;
-bool reserved2 = false;
-bool reserved3 = false;
-bool reserved4 = false;
-bool reserved5 = false;
-bool reserved6 = false;
-bool reserved7 = false;
-bool reserved8 = false;
-bool reserved9 = false;
-
-bool RestaurantTableReservation = true;
-while (RestaurantTableReservation)
+﻿class RestaurantTableReservation
 {
-    System.Console.WriteLine(" ");
-    System.Console.Write("Would you like to reserve a table? (Y/N):  ");
-    string? choicetableReservation = System.Console.ReadLine().ToUpper();
-
-    switch (choicetableReservation)
+    static void Main()
     {
+        System.Console.WriteLine("\t HELLO! WELCOME TO THE BIT BITE RESTAURANT TABLE RESERVATION SYSTEM!");
 
-        case "Y":
-            System.Console.WriteLine("\n Available Tables");
-            System.Console.WriteLine("\n Table Number \t            Status");
-            System.Console.WriteLine("      1 \t\t  {0}  \t     ", reserved1 ? "Reserved" : "Available");
-            System.Console.WriteLine("      2 \t\t  {0}  \t     ", reserved2 ? "Reserved" : "Available");
-            System.Console.WriteLine("      3 \t\t  {0}  \t     ", reserved3 ? "Reserved" : "Available");
-            System.Console.WriteLine("      4 \t\t  {0}  \t     ", reserved4 ? "Reserved" : "Available");
-            System.Console.WriteLine("      5 \t\t  {0}  \t     ", reserved5 ? "Reserved" : "Available");
-            System.Console.WriteLine("      6 \t\t  {0}  \t     ", reserved6 ? "Reserved" : "Available");
-            System.Console.WriteLine("      7 \t\t  {0}  \t     ", reserved7 ? "Reserved" : "Available");
-            System.Console.WriteLine("      8 \t\t  {0}  \t     ", reserved8 ? "Reserved" : "Available");
-            System.Console.WriteLine("      9 \t\t  {0}  \t     ", reserved9 ? "Reserved" : "Available");
+        List<bool> tables = new List<bool>();
+        int numberOfTables = 20;
 
-            System.Console.WriteLine(" ");
-            System.Console.Write("Please enter the table number:  ");
-            string? tableNumber = System.Console.ReadLine();
+        for (int i = 0; i < numberOfTables; i++)
+        {
+            tables.Add(false);
+        }
 
-            if (tableNumber == "1" && !reserved1)
-            {
-                reserved1 = true;
-            }
-            else if (tableNumber == "2" && !reserved2)
-            {
-                reserved2 = true;
-            }
-            else if (tableNumber == "3" && !reserved3)
-            {
-                reserved3 = true;
-            }
-            else if (tableNumber == "4" && !reserved4)
-            {
-                reserved4 = true;
-            }
-            else if (tableNumber == "5" && !reserved5)
-            {
-                reserved5 = true;
-            }
-            else if (tableNumber == "6" && !reserved6)
-            {
-                reserved6 = true;
-            }
-            else if (tableNumber == "7" && !reserved7)
-            {
-                reserved7 = true;
-            }
-            else if (tableNumber == "8" && !reserved8)
-            {
-                reserved8 = true;
-            }
-            else if (tableNumber == "9" && !reserved9)
-                reserved9 = true;
-            else
-            {
-                System.Console.WriteLine(" ");
-                System.Console.WriteLine("\t Table is not available. Please try again!");
-                break;
-            }
-            System.Console.WriteLine(" ");
-            System.Console.Write("Table for how many poeple?:  ");
-            int numberPeople = Convert.ToInt16(System.Console.ReadLine());
+        int tableNumber = 0;
 
+        bool RestaurantTableReservation = true;
+        while (RestaurantTableReservation)
+        {
+            DisplayMenu();
             System.Console.WriteLine(" ");
-            System.Console.Write("Please enter the desired date for this reservation (MM/DD/YYYY):  ");
-            string? dateReservation = System.Console.ReadLine();
+            System.Console.Write("Please enter the number of your choice: ");
+            string? displayMenuNumber = System.Console.ReadLine();
 
-            System.Console.WriteLine(" ");
-            System.Console.Write("\t Select a meal time:");
-            System.Console.WriteLine(" ");
-            System.Console.WriteLine("\t 1. Morning");
-            System.Console.WriteLine("\t 2. Afternoon");
-            System.Console.WriteLine("\t 3. Evening");
-            System.Console.WriteLine(" ");
-            System.Console.Write("Please enter the number of your selected meal time:  ");
-            string? mealTimeReservation = System.Console.ReadLine();
-
-            switch (mealTimeReservation)
+            switch (displayMenuNumber)
             {
                 case "1":
-                    mealTimeReservation = "Morning";
+                    System.Console.WriteLine(" ");
+                    System.Console.Write("Would you like to reserve a table? (Y/N):  ");
+                    string? choicetableReservation = System.Console.ReadLine().ToUpper();
+                    switch (choicetableReservation)
+                    {
+                        case "Y":
+                            availableTables(numberOfTables, tables);
+
+                            bool tableNumberChoice = true;
+                            while (tableNumberChoice)
+                            {
+                                System.Console.WriteLine(" ");
+                                System.Console.Write("Please enter the table number:  ");
+                                tableNumber = Convert.ToInt16(System.Console.ReadLine());
+
+                                if (tableNumber >= 1 && tableNumber <= numberOfTables && !tables[tableNumber - 1])
+                                {
+                                    tableNumberChoice = false;
+                                }
+                                else
+                                {
+                                    System.Console.WriteLine(" ");
+                                    System.Console.WriteLine("\t Table is not available. Please try again!");
+                                    continue;
+                                }
+                            }
+
+                            System.Console.WriteLine(" ");
+                            System.Console.Write("Table for how many poeple?:  ");
+                            int numberPeople = Convert.ToInt16(System.Console.ReadLine());
+
+                            System.Console.WriteLine(" ");
+                            System.Console.Write("Please enter the desired date for this reservation (MM/DD/YYYY):  ");
+                            string? dateReservation = System.Console.ReadLine();
+
+                            mealTimeReservation();
+
+                            System.Console.Write("Please enter the number of your selected meal time:  ");
+                            string? mealTimeChoice = System.Console.ReadLine();
+                            while (mealTimeChoice != "1" && mealTimeChoice != "2" && mealTimeChoice != "3")
+                            {
+                                switch (mealTimeChoice)
+                                {
+                                    case "1":
+                                        mealTimeChoice = "Morning";
+                                        break;
+
+                                    case "2":
+                                        mealTimeChoice = "Afternoon";
+                                        break;
+
+                                    case "3":
+                                        mealTimeChoice = "Evening";
+                                        break;
+
+                                    default:
+                                        System.Console.WriteLine(" ");
+                                        System.Console.WriteLine("\t Invalid Number!");
+                                        System.Console.Write("Please enter the number of meal time (1-3): ");
+                                        mealTimeChoice = System.Console.ReadLine();
+                                        continue;
+                                }
+                            }
+
+
+                            System.Console.WriteLine(" ");
+                            System.Console.Write("Do you want to confirm the booking? (Y/N): ");
+                            string confirm = System.Console.ReadLine().ToUpper();
+                            if (confirm == "Y")
+                            {
+                                tables[tableNumber - 1] = true;
+                                System.Console.WriteLine(" ");
+                                System.Console.WriteLine("\t You have successfully booked the table " + tableNumber + " for " + numberPeople + " people on " + dateReservation + ", in the " + mealTimeChoice);
+                            }
+                            break;
+
+                        default:
+                            System.Console.WriteLine(" ");
+                            System.Console.WriteLine("\t Invalid option. Please try again.");
+                            break;
+
+                        case "N":
+                            break;
+                    }
                     break;
 
                 case "2":
-                    mealTimeReservation = "Afternoon";
+                    availableTables(numberOfTables, tables);
                     break;
 
-                case "3":
-                    mealTimeReservation = "Evening";
+                case "0":
+                    System.Console.WriteLine(" ");
+                    System.Console.WriteLine("\t Exit");
+                    System.Console.WriteLine("\t Thank you for using Bit Bite Table Reservation System!");
+                    RestaurantTableReservation = false;
                     break;
 
                 default:
                     System.Console.WriteLine(" ");
-                    System.Console.WriteLine("\t Invalid Number. Please try again!");
+                    System.Console.WriteLine("\t Invalid option. Please try again.");
                     break;
+
             }
+        }
 
-            System.Console.WriteLine(" ");
-            System.Console.Write("Do you want to confirm the booking? (Y/N): ");
-            string confirm = System.Console.ReadLine().ToUpper();
-            if (confirm == "Y")
-            {
-                System.Console.WriteLine(" ");
-                System.Console.WriteLine("\t You have successfully booked the table" + tableNumber + " for " + numberPeople + " people on " + dateReservation + ", in the " + mealTimeReservation);
-            }
-            break;
+    }
 
-        case "N":
-            System.Console.WriteLine(" ");
-            System.Console.WriteLine("\t Exit");
-            System.Console.WriteLine("\t Thank you for using Table Reservation System!");
-            break;
+    private static void DisplayMenu()
+    {
+        System.Console.WriteLine(" ");
+        System.Console.WriteLine("MENU:");
+        System.Console.WriteLine("1. Reserve a table");
+        System.Console.WriteLine("2. View Reservation");
+        System.Console.WriteLine("0. Exit");
+    }
 
-        default:
-            System.Console.WriteLine(" ");
-            System.Console.WriteLine("\t Invalid option. Please try again.");
-            break;
+    private static void availableTables(int numberOfTables, List<bool> tables)
+    {
+        Console.WriteLine("Available Tables:");
+        Console.WriteLine("\tTable number   \t    Status");
+        for (int i = 0; i < numberOfTables; i++)
+        {
+            Console.WriteLine("\t     {0}   \t   {1}", i + 1, tables[i] ? "Reserved" : "Available");
+        }
+    }
+    private static void mealTimeReservation()
+    {
+        System.Console.WriteLine(" ");
+        System.Console.Write("\t Select a meal time:");
+        System.Console.WriteLine(" ");
+        System.Console.WriteLine("\t 1. Morning");
+        System.Console.WriteLine("\t 2. Afternoon");
+        System.Console.WriteLine("\t 3. Evening");
+        System.Console.WriteLine(" ");
     }
 }
